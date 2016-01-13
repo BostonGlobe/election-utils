e70eeb96b9363d7e01356cd38aa1bd218ceda54b
Gabriel Florit <gabrielflorit@gmail.com>
2016-01-13 15:56:11 -0500

    Enable hiding first name on small mobile

--- src/primaries/state-results-small-table.js
+++ src/primaries/state-results-small-table.js
@@ -19,12 +19,12 @@ function candidateRow(candidate, index, totalVoteCount) {
 		<div class='photo'><img alt='' src='assets/img/${index % 5}.png' /></div>
 		<div class='two-rows'>
 			<div class='name-and-pct'>
-				<div class='name'><span>${[first, last].join(' ')}</span></div>
-				<div class='pct'><span>${displayPct}%</span></div>
+				<div class='name'><span class='zeta'><span class='first'>${first}</span> <span class='last'>${last}</span></span></div>
+				<div class='pct'><span class='zeta'>${displayPct}%</span></div>
 			</div>
 			<div class='bar-and-votes'>
-				<div class='bar'><span style='width: ${displayPct}%'>&nbsp;</span></div>
-				<div class='votes'><span class='theta'>99,999,999 votes</span></div>
+				<div class='bar'><span style='width: ${displayPct}%' class='iota'>&nbsp;</span></div>
+				<div class='votes'><span class='iota'>9,999,999 votes</span></div>
 			</div>
 		</div>
 	</div>
