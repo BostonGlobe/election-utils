80ce23414b3d2f502c5e15c9704c1bb1070d8384
Gabriel Florit <gabrielflorit@gmail.com>
2016-01-18 15:04:53 -0500

    Remove graphic components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import stateResultsLargeTable from './primaries/state-results-large-table';
-import stateResultsSmallTable from './primaries/state-results-small-table';
-import townByTownTable from './primaries/town-by-town-table';
 import Standardize from './util/Standardize';
 
 export {
-	stateResultsLargeTable,
-	stateResultsSmallTable,
-	townByTownTable,
 	Standardize
 };
