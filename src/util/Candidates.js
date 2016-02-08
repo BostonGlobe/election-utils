6806a391ecff4c1cf3d7b10052ff60a2ea33eaf2
Gabriel Florit <gabrielflorit@gmail.com>
2016-02-07 21:27:00 -0500

    Add Candidates.getVoteCount

--- src/util/Candidates.js
+++ src/util/Candidates.js
@@ -2,6 +2,8 @@ import orderBy from 'lodash.orderby';
 
 const Candidates = {
 
+	getVoteCount: (cs) => cs.map(x => x.voteCount).reduce((x, y) => x + y, 0),
+
 	// sort candidates by vote count and ballot order
 	sort: (cs) => orderBy(cs, ['voteCount', 'ballotOrder'], ['desc', 'asc'])
 
