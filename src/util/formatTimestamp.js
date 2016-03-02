3e5663b6caf812de486b29cdcb3edc80a366e7a5
Gabriel Florit <gabrielflorit@gmail.com>
2016-03-01 21:39:09 -0500

    Add commas between date and time

--- src/util/formatTimestamp.js
+++ src/util/formatTimestamp.js
@@ -8,15 +8,12 @@ import dateFormat from 'dateformat'
  * @returns {String} the formatted timestamp
  * @example
  * import { formatTimestamp } from 'election-utils'
- * formatTimestamp(results) //=> 'Mar. 1 2:26 PM EST'
+ * formatTimestamp(results) //=> 'Mar. 1, 2:26 PM EST'
  */
 export default function formatTimestamp({ timestamp }) {
 
 	const date = new Date(timestamp)
 
-	const day = dateFormat(date, 'mmm. d')
-	const time = dateFormat(date, 'h:MM TT Z')
-
-	return [day, time].join('&nbsp;&nbsp;')
+	return dateFormat(date, 'mmm. d, h:MM TT Z')
 
 }
